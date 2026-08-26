74c3848d8f9f6595818f8822fe2505efa7bff205
钟佳铭 <zhongjm11594@fxiaoke.com>
2026-08-26 17:31:39 +0800

    feat：20260826

--- LC209.长度最小的子数组.js
+++ LC209.长度最小的子数组.js
@@ -4,16 +4,23 @@
  * @return {number}
  */
 var minSubArrayLen = function (target, nums) {
-  let sum = 0,
-    left = 0,
-    ans = nums.length + 1;
+  let left = 0;
+  let sum = 0;
+  let len = Number.POSITIVE_INFINITY;
   for (let right = 0; right < nums.length; right++) {
     sum += nums[right];
+
     while (sum >= target) {
-      ans = Math.min(ans, right - left + 1);
+      len = Math.min(len, right - left + 1);
       sum -= nums[left];
       left++;
     }
   }
-  return ans <= nums.length ? ans : 0;
+
+  return len === Number.POSITIVE_INFINITY ? 0 : len;
 };
+
+console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])); // 2
+console.log(minSubArrayLen(4, [1, 4, 4])); // 1
+console.log(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1])); // 0
+console.log(minSubArrayLen(7, [])); // 0
