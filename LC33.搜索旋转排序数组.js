2302754388a569afa2a9f06fd3535c2e4588df15
钟佳铭 <zhongjm11594@fxiaoke.com>
2026-08-27 16:15:34 +0800

    feat:20260827

--- LC33.搜索旋转排序数组.js
+++ LC33.搜索旋转排序数组.js
@@ -3,22 +3,28 @@
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
-  let left = 0,
-    right = nums.length - 1;
+var searchRotatedArray = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
 
   while (left <= right) {
-    const mid = (left + right) >> 1;
-    if (target === nums[mid]) return mid;
+    const mid = left + Math.floor((right - left) / 2);
 
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && nums[mid] > target) right = mid - 1;
+    if (nums[mid] === target) return mid;
+    if (nums[mid] >= nums[left]) {
+      if (target >= nums[left] && nums[mid] > target) right = mid - 1;
       else left = mid + 1;
     } else {
-      if (nums[right] >= target && nums[mid] < target) left = mid + 1;
+      if (target > nums[mid] && nums[right] >= target) left = mid + 1;
       else right = mid - 1;
     }
   }
 
   return -1;
 };
+
+console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 0)); // 4
+console.log(searchRotatedArray([4, 5, 6, 7, 0, 1, 2], 3)); // -1
+console.log(searchRotatedArray([1], 0)); // -1
+console.log(searchRotatedArray([1], 1)); // 0
+console.log(searchRotatedArray([5, 1, 3], 5)); // 0
