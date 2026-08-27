57ade6f54981366a0285fe08147dfa51ab331677
钟佳铭 <zhongjm11594@fxiaoke.com>
2026-08-27 16:48:55 +0800

    feat：20260827

--- LC34.在排序数组中查找元素的第一个和最后一个位置.js
+++ LC34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -5,12 +5,13 @@
  */
 var searchRange = function (nums, target) {
   const lower = (target) => {
-    let left = 0,
-      right = nums.length - 1,
-      ans = nums.length;
+    let left = 0;
+    let right = nums.length - 1;
+    let ans = nums.length;
 
     while (left <= right) {
-      const mid = (left + right) >> 1;
+      const mid = left + Math.floor((right - left) / 2);
+
       if (nums[mid] >= target) {
         ans = mid;
         right = mid - 1;
@@ -25,3 +26,9 @@ var searchRange = function (nums, target) {
   const last = lower(target + 1) - 1;
   return [first, last];
 };
+
+console.log(searchRange([5, 7, 7, 8, 8, 10], 8)); // [3, 4]
+console.log(searchRange([5, 7, 7, 8, 8, 10], 6)); // [-1, -1]
+console.log(searchRange([], 0)); // [-1, -1]
+console.log(searchRange([2, 2, 2], 2)); // [0, 2]
+console.log(searchRange([1], 1)); // [0, 0]
