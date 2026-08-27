15b128efe12150de5894a10c50b215e6a1207962
钟佳铭 <zhongjm11594@fxiaoke.com>
2026-08-27 10:53:25 +0800

    feat:20260827

--- LC11.盛最多水的容器.js
+++ LC11.盛最多水的容器.js
@@ -3,19 +3,21 @@
  * @return {number}
  */
 var maxArea = function (height) {
-  let area = 0,
-    left = 0,
-    right = height.length - 1;
+  let area = 0;
+  let left = 0;
+  let right = height.length - 1;
 
   while (left < right) {
-    currentArea = (right - left) * Math.min(height[left], height[right]);
-    area = Math.max(area, currentArea);
-    if (height[left] < height[right]) {
-      left++;
-    } else {
-      right--;
-    }
+    let currentArea = (right - left) * Math.min(height[left], height[right]);
+    area = Math.max(currentArea, area);
+    if (height[left] < height[right]) left++;
+    else right--;
   }
 
   return area;
 };
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])); // 49
+console.log(maxArea([1, 1])); // 1
+console.log(maxArea([4, 3, 2, 1, 4])); // 16
+console.log(maxArea([1])); // 0
