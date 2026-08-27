a7b96cb02ee33266b80bdd8dfd9e47efb8307334
钟佳铭 <zhongjm11594@fxiaoke.com>
2026-08-27 19:25:51 +0800

    feat：20260827

--- LC46.全排列.js
+++ LC46.全排列.js
@@ -3,29 +3,32 @@
  * @return {number[][]}
  */
 var permute = function (nums) {
-  const result = [];
-  const curr = [];
-  const isUsed = new Array(nums.length).fill(false);
-
-  // 判断这个数字是否已经在数组中，不在就加入，在就不加入，一次递归结束后把当前数字弹出
-  function dfs() {
-    if (curr.length === nums.length) {
-      result.push([...curr]);
+  const results = [];
+  const used = new Array(nums.length).fill(false);
+  const path = [];
+  function backtrack() {
+    if (path.length === nums.length) {
+      results.push([...path]);
+      return;
     }
-
     for (let i = 0; i < nums.length; i++) {
-      if (isUsed[i]) continue;
+      if (used[i]) continue;
 
-      curr.push(nums[i]);
-      isUsed[i] = true;
+      path.push(nums[i]);
+      used[i] = true;
 
-      dfs();
+      backtrack();
 
-      curr.pop();
-      isUsed[i] = false;
+      path.pop();
+      used[i] = false;
     }
   }
-  dfs();
 
-  return result;
+  backtrack();
+  return results;
 };
+
+console.log(permute([1, 2, 3]));
+// [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+console.log(permute([7])); // [[7]]
+console.log(permute([])); // [[]]
