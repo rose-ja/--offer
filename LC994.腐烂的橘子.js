e3f88ea8b3d1f869926650154e949f242ce2a2f3
钟佳铭 <zhongjm11594@fxiaoke.com>
2026-08-28 11:58:38 +0800

    feat：20260828

--- LC994.腐烂的橘子.js
+++ LC994.腐烂的橘子.js
@@ -1,32 +1,64 @@
-var orangesRotting = function(grid) {
-    const m = grid.length, n = grid[0].length;
-    let fresh = 0;
-    let q = [];
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 1) {
-                fresh++; // 统计新鲜橘子个数
-            } else if (grid[i][j] === 2) {
-                q.push([i, j]); // 一开始就腐烂的橘子
-            }
+var orangesRotting = function (grid) {
+  let fresh = 0;
+  let q = [];
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) fresh++;
+      else if (grid[i][j] === 2) q.push([i, j]);
+    }
+  }
+
+  let time = 0;
+  while (q.length && fresh) {
+    const temp = q;
+    q = [];
+    for (const [x, y] of temp) {
+      for (const [i, j] of [
+        [x - 1, y],
+        [x + 1, y],
+        [x, y - 1],
+        [x, y + 1],
+      ]) {
+        if (
+          i < 0 ||
+          j < 0 ||
+          i >= grid.length ||
+          j >= grid[0].length ||
+          grid[i][j] !== 1
+        )
+          continue;
+        else {
+          fresh--;
+          grid[i][j] = 2;
+          q.push([i, j]);
         }
+      }
     }
 
-    let ans = 0;
-    while (fresh && q.length) {
-        ans++; // 经过一分钟
-        const tmp = q;
-        q = [];
-        for (const [x, y] of tmp) { // 已经腐烂的橘子
-            for (const [i, j] of [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]) { // 四方向
-                if (0 <= i && i < m && 0 <= j && j < n && grid[i][j] === 1) { // 新鲜橘子
-                    fresh--;
-                    grid[i][j] = 2; // 变成腐烂橘子
-                    q.push([i, j]);
-                }
-            }
-        }
-    }
+    time++;
+  }
 
-    return fresh ? -1 : ans;
+  return fresh ? -1 : time;
 };
+
+console.log(
+  orangesRotting([
+    [2, 1, 1],
+    [0, 1, 1],
+    [0, 0, 1],
+  ]),
+); // 4
+console.log(
+  orangesRotting([
+    [2, 1, 1],
+    [0, 1, 1],
+    [1, 0, 1],
+  ]),
+); // -1
+console.log(orangesRotting([[0, 2]])); // 0
+console.log(
+  orangesRotting([
+    [1, 1],
+    [1, 1],
+  ]),
+); // -1
