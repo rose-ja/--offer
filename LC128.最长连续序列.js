e5e94af4ceb0aa77c64fe126330a75fc22a484f3
钟佳铭 <zhongjm11594@fxiaoke.com>
2026-09-01 14:23:35 +0800

    feat:20260901

--- LC128.最长连续序列.js
+++ LC128.最长连续序列.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-  const s = new Set(nums);
+  const set = new Set(nums);
   let ans = 0;
 
-  for (let x of s) {
-    if (s.has(x - 1)) continue;
-    let y = x + 1;
-    while (s.has(y)) {
-      y++;
+  for (let num of nums) {
+    if (set.has(num - 1)) continue;
+
+    let next = num + 1;
+    while (set.has(next)) {
+      next++;
     }
-    ans = Math.max(ans, y - x);
-    if (ans * 2 >= s.size) break;
+    ans = Math.max(ans, next - num);
+    if (ans * 2 >= set.size) break; // 如果我们发现其中一条链的长度至少为 m/2,不可能还有一条大于m/2的链
   }
-  
   return ans;
 };
